6f06249f34d3f341a55503e80258aea762be8f47
Pretwolk <git@pretwolk.nl>
2017-10-08 04:19:09 +0000

    Added config features

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3
 
-import json, re, time, sys
+import json, re, time, sys,yaml
 
 from datetime import datetime,timezone
 from elasticsearch import Elasticsearch
@@ -9,9 +9,16 @@ from flask import abort,redirect,url_for,request,jsonify
 
 class ContentSecurityPolicyReport():
 
+    config = {}
+
     def __init__(self):
+        self.read_config()
         self.set_es()
         pass
+
+    def read_config(self):
+        with open('config/config.yaml') as file_config:
+            self.config = yaml.load(file_config)
 
     def sanitize(self,in_str):
         regex = r'[^\w\d$]'
@@ -30,7 +37,7 @@ class ContentSecurityPolicyReport():
 
     def set_es(self):
         try:
-            self.es = Elasticsearch([{'host': 'frsys04','port':9200}])
+            self.es = Elasticsearch([{'host': self.config['es']['host'],'port':self.config['es']['port']}])
         except:
             error = str(sys.exc_info())
             self.json_response(503)
@@ -51,6 +58,8 @@ class ContentSecurityPolicyReport():
         if request.headers.get('Content-Type') != "application/csp-report":
             self.json_response(403)
         if not re.match(r'^Mozilla.*', request.headers.get('User-Agent')):
+            self.json_response(403)
+        if client_id not in self.config['client_ids']:
             self.json_response(403)
 
         csp_request = json.loads(str(request.data,'utf-8'), encoding='utf-8')
@@ -77,5 +86,5 @@ def apiv1csp(client_id):
 
 if __name__ == "__main__":
     cspr = ContentSecurityPolicyReport()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host=cspr.config['network']['host'], port=cspr.config['network']['port'])
 
